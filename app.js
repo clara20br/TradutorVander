9bb4dbf7e827b67de1cf3a07699b3c60704cab21
Rosa <rosalinda.silva@escola.pr.gov.br>
2026-03-31 08:09:48 -0300

    conexão com a api

---
+++ app.js
@@ -0,0 +1,16 @@
+let inputTexto = document.querySelector(".texto");
+let idioma = document.querySelector(".idioma")
+let texto_traducao = document.querySelector(".texto-traducao")
+
+
+async function traduzir(){
+
+
+   let url = "https://api.mymemory.translated.net/get?q=Bom%20trabalho!&langpair=pt|en";
+
+   let resposta  = await fetch(url);
+  
+   let traducao = await resposta.json(); 
+
+   console.log(traducao.responseData.translatedText);
+}
